75842e70ff34ff70fbd6a58e1280d492dd702d6e
Antonio Filho <designerdeunhasdefinitivo@gmail.com>
2022-03-13 14:40:33 -0300

    install gorm and mysql

--- fiber-project/main.go
+++ fiber-project/main.go
@@ -1,15 +1,29 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+	var dsn = "root:mysql0401@/fluent_admin?charset=utf8mb4&parseTime=True&loc=Local"
+	var v = "Não conseguiu conectar ao banco de dados"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(v)
+	}
+	fmt.Println("Conexão OK!")
+	fmt.Println(db)
+
 	app := fiber.New()
 
-	app.Get("/", home)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
 
 	app.Listen(":3000")
-}
-
-func home(c *fiber.Ctx) error {
-	return c.SendString("Hello, World 👋!")
 }
