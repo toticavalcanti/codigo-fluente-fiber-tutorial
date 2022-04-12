4849b8f33fd19daa561935721ab29658e1528ac3
Antonio Filho <designerdeunhasdefinitivo@gmail.com>
2022-04-12 14:14:12 -0300

    home function

--- fiber-project/main.go
+++ fiber-project/main.go
@@ -21,9 +21,11 @@ func main() {
 
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	app.Get("/", home)
 
 	app.Listen(":3000")
+}
+
+func home(c *fiber.Ctx) error {
+	return c.SendString("Hello, World 👋!")
 }
