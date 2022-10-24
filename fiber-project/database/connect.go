5a68b4f2107ad44b40e31eb3e5e0f895851fe8a5
toticavalcanti <toticavalcanti@hotmail.com>
2022-10-24 17:37:51 -0300

    user migration

--- fiber-project/database/connect.go
+++ fiber-project/database/connect.go
@@ -3,6 +3,8 @@ package database
 import (
 	"fmt"
 
+	"fiber-project/models"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,10 +12,12 @@ import (
 func Connect() {
 	var dsn = "toticavalcanti:mysql1234@/fluent_admin?charset=utf8mb4&parseTime=True&loc=Local"
 	var v = "Não conseguiu conectar ao banco de dados"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(v)
 	}
+
+	connection.AutoMigrate(&models.User{})
 	fmt.Println("Conexão OK!")
 }
