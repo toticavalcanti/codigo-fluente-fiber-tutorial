2078883fea17bf52099e05ca2c98709468f1e551
toticavalcanti <toticavalcanti@hotmail.com>
2024-11-13 20:36:01 -0300

    fix: adjust CORS configuration and add middleware for debugging Origin header

--- fiber-project/main.go
+++ fiber-project/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fiber-project/database"
 	"fiber-project/routes"
-	"fmt"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,39 +10,24 @@ import (
 )
 
 func main() {
-	// Conecta ao banco de dados
 	database.Connect()
 	app := fiber.New()
 
-	// Middleware CORS
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: func() string {
-			origin := os.Getenv("CORS_ORIGIN")
-			if origin == "" {
-				return "*"
-			}
-			return origin
-		}(),
+		AllowOriginsFunc: func(origin string) bool {
+			return origin != ""
+		},
 		AllowMethods:     "GET,POST,PUT,DELETE",
-		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
 	}))
 
-	// Middleware para logar o cabeçalho "Origin"
-	app.Use(func(c *fiber.Ctx) error {
-		fmt.Printf("Request Origin: %s\n", c.Get("Origin"))
-		return c.Next()
-	})
-
-	// Configuração de rotas
 	routes.Setup(app)
 
-	// Configuração da porta
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 
-	// Inicia o servidor
 	app.Listen(":" + port)
 }
