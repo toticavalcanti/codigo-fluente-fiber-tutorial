7c8a3e7a1d137a92f2d864b871b90d24a6cdf51a
toticavalcanti <toticavalcanti@hotmail.com>
2024-11-21 20:12:46 -0300

    fix: add expiration time to password reset tokens
    - Add ExpiresAt field when creating password reset tokens
    - Set token expiration to 1 hour from creation
    - Fix token validation in reset flow

--- fiber-project/services/forgot_service.go
+++ fiber-project/services/forgot_service.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"net/smtp"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
@@ -25,8 +26,9 @@ func Forgot(c *fiber.Ctx) error {
 	// Gera um token aleatório para redefinição de senha
 	token := RandStringRunes(12)
 	passwordReset := models.PasswordReset{
-		Email: data["email"],
-		Token: token,
+		Email:     data["email"],
+		Token:     token,
+		ExpiresAt: time.Now().Add(1 * time.Hour),
 	}
 
 	// Salva o token no banco de dados
